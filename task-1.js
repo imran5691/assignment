900e1946e0c83405e502053ef9302134c292a6c3
imran5691 <imranhossen325948@gmail.com>
2023-11-29 09:19:15 +0600

    assignment

---
+++ task-1.js
@@ -0,0 +1,14 @@
+var f_Num=Number(prompt("Enter your First number :"));
+
+var l_Num=Number(prompt("Enter your Last number :"));
+
+
+document.write("sum Number is  :" +(f_Num+l_Num+"<br/><br/>"));
+
+document.write("Sub  Number is  :" +(f_Num-l_Num+"<br/><br/>"));
+
+document.write("Mul Number is  :" +(f_Num*l_Num+"<br/><br/>"));
+
+document.write("Division Number is  :" +(f_Num/l_Num+"<br/><br/>"));
+
+document.write("Modulus Number is  :" +(f_Num%l_Num+"<br/><br/>"));
